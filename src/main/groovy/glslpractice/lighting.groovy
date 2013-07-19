ea910bfeaf155933f6122d5d6d7d185034ed90b0
urasa <ursk4git@live.jp>
2013-07-19 13:25:24 +0900

    phong shadingの実装

--- src/main/groovy/glslpractice/lighting.groovy
+++ src/main/groovy/glslpractice/lighting.groovy
@@ -61,7 +61,7 @@ def display = { GLAutoDrawable drawable ->
         glShadeModel GL_SMOOTH
         //glShadeModel GL_FLAT
         // シェーダープログラムの選択
-        glUseProgram shaders.simplifiedGouraud
+        glUseProgram shaders.phong
         // glUseProgram 0
         glClear GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
         glMatrixMode GL_MODELVIEW
