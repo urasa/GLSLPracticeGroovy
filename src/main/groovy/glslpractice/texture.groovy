68fcb4ac235ae45528e7d28abc9a00b48e888d04
urasa <ursk4git@live.jp>
2013-07-22 12:33:07 +0900

    テクスチャを貼り付ける対象ができた

--- src/main/groovy/glslpractice/texture.groovy
+++ src/main/groovy/glslpractice/texture.groovy
@@ -13,26 +13,59 @@ import javax.swing.WindowConstants
 import com.jogamp.opengl.util.FPSAnimator
 import com.jogamp.opengl.util.gl2.GLUT
 
+def makeTextureData256(int sNum, int tNum) {
+	
+}
+
 GLUT glut = new GLUT()
 GLU glu = new GLU()
 int fps = 60
+FPSAnimator animator = new FPSAnimator(fps)
 Camera camera = new Camera(0d, 0d, 100d, 0d, 0d, 0d)
-
+def displaylists = [:]
 
 ///// GLEventListener Implementations ///////////////////////
 def init = { GLAutoDrawable drawable ->
     drawable.getGL().getGL2().with { gl2 ->
+		glEnable GL_DEPTH_TEST
+		glClearColor(0.2f, 0.2f, 0.2f, 0.0f)
 
+        displaylists.grid = glGenLists(1)
+        glNewList(displaylists.grid, GL_COMPILE)
+        GLSLUtils.drawGrid gl2
+		glRotated(90d, 0d, 1d, 0d)
+        GLSLUtils.drawGrid gl2
+        glEndList()
     }
 }
+final def cycleTime = 5d
+final def rotationAnglePerFrame = (360d / fps) / cycleTime
 def display = { GLAutoDrawable drawable ->
     drawable.getGL().getGL2().with { gl2 ->
+		glClear GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
+		glMatrixMode GL_MODELVIEW
+		glLoadIdentity()
+		camera.look glu
+
+		glPushMatrix()
+		glRotated(rotationAnglePerFrame*animator.getTotalFPSFrames(), 0d, 1d, 0d)
+		glut.glutSolidSphere(30d, 20, 20)
+		glTranslated(40d, 0d, 0d)
+		glRotated(rotationAnglePerFrame*animator.getTotalFPSFrames(), 0d, 1d, 0d)
+		glut.glutSolidCube 10f
+		glPopMatrix()
+
+		glUseProgram 0
+		glColor3f(0.1f, 0.1f, 0.1f)
+		glCallList displaylists.grid
 
     }
 }
 def reshape = { GLAutoDrawable drawable, int x, int y, int width, int height ->
     drawable.getGL().getGL2().with { gl2 ->
-
+		glMatrixMode GL_PROJECTION
+		glLoadIdentity()
+		glu.gluPerspective(30f, (float)width/(float)height, 1f, 10000f);
     }
 }
 
@@ -45,7 +78,7 @@ caps.with {
 }
 
 new SwingBuilder().edt {
-    frame(title:'', size:[400, 400], show:true,
+    frame(title:'テクスチャ', size:[400, 400], show:true,
     defaultCloseOperation:WindowConstants.EXIT_ON_CLOSE) {
         borderLayout()
         new GLJPanel(caps).with { panel ->
@@ -54,8 +87,9 @@ new SwingBuilder().edt {
             addGLEventListener listener
             addMouseListener camera
             addMouseMotionListener camera
+            animator.add panel
             widget panel
-            new FPSAnimator(panel, fps).start()
         }
     }.addKeyListener camera
 }
+animator.start()
