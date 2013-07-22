dd2288e11946eef508c3f68a658cf7e9e596079e
urasa <ursk4git@live.jp>
2013-07-22 11:36:22 +0900

    テクスチャ処理の作成を開始

---
+++ src/main/groovy/glslpractice/texture.groovy
@@ -0,0 +1,61 @@
+package glslpractice
+
+import groovy.swing.SwingBuilder
+
+import javax.media.opengl.GLAutoDrawable
+import javax.media.opengl.GLCapabilities
+import javax.media.opengl.GLEventListener
+import javax.media.opengl.GLProfile
+import javax.media.opengl.awt.GLJPanel
+import javax.media.opengl.glu.GLU
+import javax.swing.WindowConstants
+
+import com.jogamp.opengl.util.FPSAnimator
+import com.jogamp.opengl.util.gl2.GLUT
+
+GLUT glut = new GLUT()
+GLU glu = new GLU()
+int fps = 60
+Camera camera = new Camera(0d, 0d, 100d, 0d, 0d, 0d)
+
+
+///// GLEventListener Implementations ///////////////////////
+def init = { GLAutoDrawable drawable ->
+    drawable.getGL().getGL2().with { gl2 ->
+
+    }
+}
+def display = { GLAutoDrawable drawable ->
+    drawable.getGL().getGL2().with { gl2 ->
+
+    }
+}
+def reshape = { GLAutoDrawable drawable, int x, int y, int width, int height ->
+    drawable.getGL().getGL2().with { gl2 ->
+
+    }
+}
+
+
+///// Frame Construction ////////////////////////////////////
+GLCapabilities caps = new GLCapabilities(GLProfile.getDefault())
+caps.with {
+    setNumSamples 4
+    setSampleBuffers true
+}
+
+new SwingBuilder().edt {
+    frame(title:'', size:[400, 400], show:true,
+    defaultCloseOperation:WindowConstants.EXIT_ON_CLOSE) {
+        borderLayout()
+        new GLJPanel(caps).with { panel ->
+            GLEventListener listener = GLEventListenerFactory.createListener(
+                    init: init, display: display, reshape: reshape)
+            addGLEventListener listener
+            addMouseListener camera
+            addMouseMotionListener camera
+            widget panel
+            new FPSAnimator(panel, fps).start()
+        }
+    }.addKeyListener camera
+}
