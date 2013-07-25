bd492bb568922898b3efd48181827cc8b99cbc25
urasa <ursk4git@live.jp>
2013-07-25 17:21:49 +0900

    クラス名Textureの衝突を解消、チェック柄にできた

--- src/main/groovy/glslpractice/texture.groovy
+++ src/main/groovy/glslpractice/texture.groovy
@@ -2,7 +2,6 @@ package glslpractice
 
 import groovy.swing.SwingBuilder
 
-import java.nio.DoubleBuffer;
 import java.nio.IntBuffer
 
 import javax.media.opengl.GLAutoDrawable
@@ -16,23 +15,32 @@ import javax.swing.WindowConstants
 import com.jogamp.opengl.util.FPSAnimator
 import com.jogamp.opengl.util.gl2.GLUT
 
+
+/**
+ * 正方形のチェック柄のテクスチャデータを作成する
+ * @param size テクスチャの一辺のサイズ
+ * @param sNum チェック柄の横方向の分割数
+ * @param tNum チェック柄の縦方向の分割数
+ * @return チェック柄のデータ
+ */
 def makeTextureData(int size, int sNum, int tNum) {
-	final int WIDTH = size
-	final int HEIGHT = size
-	int[] data = new int[WIDTH*HEIGHT]
-	HEIGHT.times { j ->
-		WIDTH.times { i ->
-			int n = j*WIDTH+i
-			int val = ((i.intdiv(sNum)+1)%2)*255
-			val = 255 + (val << 8) + (val << 16) + (val << 24)
-			data[n] = val
-//			println "[j:$j,i:$i]"
-//			3.times { data[n+it] = val }
-//			data[n+3] = 255
-		}
-	}
-	// TODO ボーダーからチェックへんこうする
-	return data
+    final int WIDTH = size
+    final int HEIGHT = size
+    int[] data = new int[WIDTH*HEIGHT]
+    final int black = 0x000000ff
+    final int white = 0xffffffff
+    HEIGHT.times { int j ->
+        WIDTH.times { i ->
+            int n = j*WIDTH+i
+            if (i.intdiv(sNum)%2 == j.intdiv(tNum)%2) {
+                data[n] =  white
+            }
+            else {
+                data[n] =  black
+            }
+        }
+    }
+    return data
 }
 
 GLUT glut = new GLUT()
@@ -42,113 +50,113 @@ FPSAnimator animator = new FPSAnimator(fps)
 Camera camera = new Camera(0d, 0d, 100d, 0d, 0d, 0d)
 def shaders = [:]
 def displaylists = [:]
-class Texture {
-	int name
-	int[] data
-	Texture(Map map) {
-		this.name = map.name
-		this.data = map.data
-	}
+class TextureData {
+    int name
+    int[] data
+    TextureData(Map map) {
+        this.name = map.name
+        this.data = map.data
+    }
 }
-def textures = [:].withDefault { new Texture(name:0, data:null) }
+def textures = [:].withDefault { new TextureData(name:0, data:null) }
 
 ///// GLEventListener Implementations ///////////////////////
 def init = { GLAutoDrawable drawable ->
     drawable.getGL().getGL2().with { gl2 ->
-		glEnable GL_DEPTH_TEST
-		glClearColor(0.2f, 0.2f, 0.2f, 0.0f)
+        glEnable GL_DEPTH_TEST
+        glClearColor(0.2f, 0.2f, 0.2f, 0.0f)
 
         displaylists.grid = glGenLists(1)
         glNewList(displaylists.grid, GL_COMPILE)
         GLSLUtils.drawGrid gl2
-		glRotated(90d, 0d, 1d, 0d)
+        glRotated(90d, 0d, 1d, 0d)
         GLSLUtils.drawGrid gl2
         glEndList()
-		
-		shaders.tex = GLSLUtils.createShader gl2,
-				'glsl/texture/texture.vert', 'glsl/texture/texture.frag'
-		
-		textures.tex.data = makeTextureData(64, 8, 8)
-//		println textures.tex.data
-		glPixelStorei GL_UNPACK_ALIGNMENT, 1
-		int[] texname = [-1]
-		glGenTextures(1, IntBuffer.wrap(texname))
-		textures.tex.name = texname[0]
-		println "texname: ${texname}, textures.tex.name: ${textures.tex.name}"
-		glBindTexture GL_TEXTURE_2D, textures.tex.name
-		glTexImage2D GL_TEXTURE_2D, 0, GL_RGBA,
-				64, 64, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, IntBuffer.wrap(textures.tex.data)
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glBindTexture GL_TEXTURE_2D, 0
 
-//		/* 頂点のオブジェクト空間における座標値をテクスチャ座標に使う */
-//		glTexGeni GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR
-//		glTexGeni GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR
-//		/* テクスチャ座標生成関数の設定 */
-//		glTexGendv GL_S, GL_OBJECT_PLANE, DoubleBuffer.wrap([1.0, 0.0, 0.0] as double[])
-//		glTexGendv GL_T, GL_OBJECT_PLANE, DoubleBuffer.wrap([0.0, 1.0, 0.0] as double[])
-		int[] a = [0]
-		glGetIntegerv(GL_MAX_TEXTURE_UNITS, IntBuffer.wrap(a))
-		println "max texture units: $a"
+        shaders.tex = GLSLUtils.createShader gl2,
+                'glsl/texture/texture.vert', 'glsl/texture/texture.frag'
+
+        textures.tex.data = makeTextureData(64, 8, 8)
+        //		println textures.tex.data
+        glPixelStorei GL_UNPACK_ALIGNMENT, 1
+        int[] texname = [-1]
+        glGenTextures(1, IntBuffer.wrap(texname))
+        textures.tex.name = texname[0]
+        println "texname: ${texname}, textures.tex.name: ${textures.tex.name}"
+        glBindTexture GL_TEXTURE_2D, textures.tex.name
+        glTexImage2D GL_TEXTURE_2D, 0, GL_RGBA,
+                64, 64, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, IntBuffer.wrap(textures.tex.data)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glBindTexture GL_TEXTURE_2D, 0
+
+        //		/* 頂点のオブジェクト空間における座標値をテクスチャ座標に使う */
+        //		glTexGeni GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR
+        //		glTexGeni GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR
+        //		/* テクスチャ座標生成関数の設定 */
+        //		glTexGendv GL_S, GL_OBJECT_PLANE, DoubleBuffer.wrap([1.0, 0.0, 0.0] as double[])
+        //		glTexGendv GL_T, GL_OBJECT_PLANE, DoubleBuffer.wrap([0.0, 1.0, 0.0] as double[])
+        int[] a = [0]
+        glGetIntegerv(GL_MAX_TEXTURE_UNITS, IntBuffer.wrap(a))
+        println "max texture units: $a"
     }
 }
 final def cycleTime = 5d
 final def rotationAnglePerFrame = (360d / fps) / cycleTime
 def display = { GLAutoDrawable drawable ->
     drawable.getGL().getGL2().with { gl2 ->
-		glActiveTexture GL_TEXTURE0
-		glBindTexture GL_TEXTURE_2D, textures.tex.name
-		
-		glUseProgram shaders.tex
-		def samplerLocation = glGetUniformLocation shaders.tex, 'sampler'
-		glUniform1i samplerLocation, 0
-		
-		glClear GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
-		glMatrixMode GL_MODELVIEW
-		glLoadIdentity()
-		camera.look glu
-		
-		glColor3f(0.7f, 0.1f, 0.1f)
-		
-		glEnable GL_TEXTURE_2D
-		glBegin GL_TRIANGLES
-		glNormal3d(0d, 0d, 1d)
-		glTexCoord2d(0d, 0d); glVertex3d(-50d, 0d, 0d)
-		glTexCoord2d(1d, 0d); glVertex3d( 50d, 0d, 0d)
-		glTexCoord2d(0d, 1d); glVertex3d(-50d, 100d, 0d)
-		glEnd()
-		glDisable GL_TEXTURE_2D
-		
-		/* テクスチャ座標の自動生成を有効にする */
-//		glEnable GL_TEXTURE_GEN_S
-//		glEnable GL_TEXTURE_GEN_T
-		
-		glPushMatrix()
-		glRotated(rotationAnglePerFrame*animator.getTotalFPSFrames(), 0d, 1d, 0d)
-//		glut.glutSolidSphere(30d, 20, 20)
-		glTranslated(40d, 0d, 0d)
-		glRotated(rotationAnglePerFrame*animator.getTotalFPSFrames(), 0d, 1d, 0d)
-		glut.glutSolidCube 10f
-		glPopMatrix()
+        glActiveTexture GL_TEXTURE0
+        glBindTexture GL_TEXTURE_2D, textures.tex.name
 
-		/* テクスチャ座標の自動生成を無効にする */
-//		glDisable GL_TEXTURE_GEN_S
-//		glDisable GL_TEXTURE_GEN_T
-	  
-		glUseProgram 0
-		glColor3f(0.1f, 0.1f, 0.1f)
-		glCallList displaylists.grid
+        glUseProgram shaders.tex
+        def samplerLocation = glGetUniformLocation shaders.tex, 'sampler'
+        glUniform1i samplerLocation, 0
+
+        glClear GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
+        glMatrixMode GL_MODELVIEW
+        glLoadIdentity()
+        camera.look glu
+
+        glColor3f(0.7f, 0.1f, 0.1f)
+
+        glEnable GL_TEXTURE_2D
+        glBegin GL_TRIANGLES
+        glNormal3d(0d, 0d, 1d)
+        glTexCoord2d(0d, 0d); glVertex3d(-50d, 0d, 0d)
+        glTexCoord2d(1d, 0d); glVertex3d( 50d, 0d, 0d)
+        glTexCoord2d(0d, 1d); glVertex3d(-50d, 100d, 0d)
+        glEnd()
+        glDisable GL_TEXTURE_2D
+
+        /* テクスチャ座標の自動生成を有効にする */
+        //		glEnable GL_TEXTURE_GEN_S
+        //		glEnable GL_TEXTURE_GEN_T
+
+        glPushMatrix()
+        glRotated(rotationAnglePerFrame*animator.getTotalFPSFrames(), 0d, 1d, 0d)
+        //		glut.glutSolidSphere(30d, 20, 20)
+        glTranslated(40d, 0d, 0d)
+        glRotated(rotationAnglePerFrame*animator.getTotalFPSFrames(), 0d, 1d, 0d)
+        glut.glutSolidCube 10f
+        glPopMatrix()
+
+        /* テクスチャ座標の自動生成を無効にする */
+        //		glDisable GL_TEXTURE_GEN_S
+        //		glDisable GL_TEXTURE_GEN_T
+
+        glUseProgram 0
+        glColor3f(0.1f, 0.1f, 0.1f)
+        glCallList displaylists.grid
 
     }
 }
 def reshape = { GLAutoDrawable drawable, int x, int y, int width, int height ->
     drawable.getGL().getGL2().with { gl2 ->
-		glMatrixMode GL_PROJECTION
-		glLoadIdentity()
-		glu.gluPerspective(30f, (float)width/(float)height, 1f, 10000f);
+        glMatrixMode GL_PROJECTION
+        glLoadIdentity()
+        glu.gluPerspective(30f, (float)width/(float)height, 1f, 10000f);
     }
 }
 
